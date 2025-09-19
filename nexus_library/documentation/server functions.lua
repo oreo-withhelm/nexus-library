0e6da06586bcb6c39c66e8b835c71e124e4745a1
Oreo <62381889+oreo-withhelm@users.noreply.github.com>
2025-09-19 18:38:20 +0100

    Delete nexus_library directory

--- nexus_library/documentation/server functions.lua
+++
@@ -1,7 +0,0 @@
--- print a chat message
--- tbl = {color_white, "hello there", color_black, " what are you doing!"}
-Nexus:ChatMessage(ply, tbl)
-
--- if you want it to be in the users language and run :GetPhrase(str, addon)
--- make str the key of the language string and addonPhrase as the addon name
-Nexus:Notify(ply, notification_type, seconds, str, addonPhrase)
